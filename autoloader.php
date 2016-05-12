1d404555c8f6b57b1e91ee873cd5e9737cfbf83b
Thomas Baker <bakert@fb.com>
2016-05-12 21:37:54 +0200

    Allow autoloading of players.

--- autoloader.php
+++ autoloader.php
@@ -2,7 +2,7 @@
 
 class Autoloader {
   public function load($name) {
-    foreach (['', 'exceptions/'] as $dir) {
+    foreach (['', 'exceptions/', 'players/'] as $dir) {
       $path = __DIR__ . '/' . $dir . mb_strtolower($name) . '.php';
       if (file_exists($path)) {
         require_once $path;
