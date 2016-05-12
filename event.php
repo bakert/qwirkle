016fd99d5ab61c123e361b59c966800df2e2fff2
Thomas Baker <bakert@fb.com>
2016-05-12 21:42:13 +0200

    Slightly different way of echo-ing in Event.

--- event.php
+++ event.php
@@ -4,25 +4,35 @@ class Event {
   public function draw(Player $player, array $tiles) {
     Assert::type($tiles, Tile);
     $hand = new Hand($tiles); 
-    echo "{$player} draws {$hand}";
+    $this->p("{$player} draws {$hand}");
     if (count($tiles) < $player->hand()->size()) {
-      echo ", hand is {$player->hand()}";
+      $this->p(", hand is {$player->hand()}");
     }
-    echo "\n\n";
+    $this->l()->l();
   }
 
   public function move(Player $player, Move $move, $score, Board $board, Scores $scores) {
     if (count($move->placements()) > 0) {
-      echo "{$player} plays {$move} for {$score}\n";
-      echo "\n{$board}";
-      echo "\n{$scores}";
+      $this->l("{$player} plays {$move} for {$score}");
+      $this->l()->p($board);
+      $this->l()->p($scores);
     } else {
-      echo "{$player} changes his letters.\n";
+      $this->l("$player changes his letters. Hand is {$player->hand()}");
     }
-    echo "\n";
+    $this->l();
   }
 
   public function __call($f, array $args) {
-    echo "$f\n";
+    $this->l("$f");
+  }
+
+  private function p($s) {
+    echo $s;
+    return $this;
+  }
+
+  private function l($s = '') {
+    echo "$s\n";
+    return $this;
   }
 }
