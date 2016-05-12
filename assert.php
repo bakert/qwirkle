c2a10a8f98f18a38e7c3701ace4ea7889b3a7001
Thomas Baker <bakert@fb.com>
2016-05-12 21:37:23 +0200

    Allow for subclasses when typechecking arrays.

--- assert.php
+++ assert.php
@@ -3,7 +3,7 @@
 class Assert {
   public static function type(array $a, $class) {
     foreach ($a as $item) {
-      if (get_class($item) !== $class) {
+      if (get_class($item) !== $class && !is_subclass_of($item, $class)) {
         throw new TypeException("$item is not of type $class");
       }
     }
