b40651141499ad4ed78d1a69db00b52438e35ec9
Thomas Baker <bakert@fb.com>
2016-05-14 05:14:48 +0200

    First test game for perf improvement checking.

--- exec.php
+++ exec.php
@@ -2,9 +2,13 @@
 
 class Exec {
   public function go() {
-    $players = [new Player('Qwirky'), new Player('Kwirkster'), new Player('Mr. Three')];
-    $game = new Game($players);
+    $players = [
+      new Player('Qwirky'),
+      new Player('Kwirkster'),
+      new Player('Mr. Three'),
+      new CautiousPlayer('Cautious Player')
+    ];
+    $game = new Game($players, unserialize(TestGames::FIRST));
     $game->go();
   }
 }
-
