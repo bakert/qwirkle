f0ae12a812becb66a58cd9d87a67347446899132
Thomas Baker <bakert@fb.com>
2016-05-14 10:11:12 +0200

    Some code to run lots of games and total the results.

--- exec.php
+++ exec.php
@@ -3,12 +3,36 @@
 class Exec {
   public function go() {
     $players = [
-      new Player('Qwirky'),
-      new Player('Kwirkster'),
-      new Player('Mr. Three'),
-      new CautiousPlayer('Cautious Player')
+      // new Player('Qwirky'),
+      // new Player('Kwirkster'),
+      // new Player('Mr. Three'),
+      new Player('bakert'),
+      new Player('Syl'),
+      new Player('Fra')
     ];
     $game = new Game($players, unserialize(TestGames::FIRST));
     $game->go();
+    exit;
+
+    $players = [
+      // new Player('Qwirky'),
+      // new Player('Kwirkster'),
+      // new Player('Mr. Three'),
+      new CautiousPlayer('bakert'),
+      new CautiousPlayer('Syl'),
+      new Player('Fra')
+    ];
+    $totals = [];
+    for ($i = 0; $i < 100; $i++) {
+      $game = new Game($players/*, unserialize(TestGames::FIRST)*/);
+      $game->go();
+      foreach ($game->scores()->scores() as $score) {
+        if (!isset($totals[(string)$score->player()])) {
+          $totals[(string)$score->player()] = 0;
+        }
+        $totals[(string)$score->player()] += $score->score();
+      }
+      print_r($totals);
+    }
   }
 }
